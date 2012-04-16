cf7258cfb1d070645055f6cdc3784440907241fe
MAYU <mtsuzaka@rb3.so-net.ne.jp>
2012-04-17 03:33:39 +0900

    Update TabNotification/SecondViewController.m

--- TabNotification/SecondViewController.m
+++ TabNotification/SecondViewController.m
@@ -22,37 +22,37 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
-    //カウンタ初期値
+    //カウンタ初期値、Counter initial value
     count = 0;
     
-    //通知を受信して-(void)receiveNotification1:を実行
+    //通知を受信して-(void)receiveNotification1:を実行、notice is received and -(void) is performed
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(receiveNotification1:) name:@"P1" object:nil];
-    //通知を受信して-(void)receiveNotification2:を実行
+    //通知を受信して-(void)receiveNotification2:を実行、notice is received and -(void) is performed
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(receiveNotification2:) name:@"P2" object:nil];
-  //通知を受信して-(void)receiveNotification3:を実行
+  //通知を受信して-(void)receiveNotification3:を実行、notice is received and -(void) is performed
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(receiveNotification3:) name:@"P3" object:nil];
-     //通知を受信して-(void)receiveNotification4:を実行
+     //通知を受信して-(void)receiveNotification4:を実行、notice is received and -(void) is performed
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(receiveNotification4:) name:@"P4" object:nil];
      
 }
 
-//カウンタの動作を記述
+//カウンタの動作を記述、Operation of counter
 -(void)receiveNotification1:(NSNotification *) notification {
     count++;
     countText.text = [NSString stringWithFormat:@"%d",count];
 }
-//メッセージ表示の動作を記述
+//メッセージ表示の動作を記述、Operation of message
 -(void)receiveNotification2:(NSNotification *) notification {
     mText.text = [NSString stringWithFormat:@"Notification OK"];
 }
-//スライダの値を表示する動作を記述
+//スライダの値を表示する動作を記述、Operation of slider
 -(void)receiveNotification3:(NSNotification *) notification {
     NSString *valueF = [[notification userInfo] objectForKey:@"KEY1"];
     sliderValue.text = valueF;
     
 }
 
-//テキストを表示する動作を記述
+//テキストを表示する動作を記述、Operation of text view
 -(void)receiveNotification4:(NSNotification *) notification {
     NSString *textV = [[notification userInfo] objectForKey:@"KEY2"];
     textF.text = textV;
