dbb9beb7a74691fd16c7decd24166df8c2253a62
MAYU <mtsuzaka@rb3.so-net.ne.jp>
2012-04-17 03:24:46 +0900

    Update TabNotification/FirstViewController.h

--- TabNotification/FirstViewController.h
+++ TabNotification/FirstViewController.h
@@ -9,18 +9,18 @@
 #import <UIKit/UIKit.h>
 
 @interface FirstViewController : UIViewController
-//スライダの値
+//スライダの値、sliderValue
 @property (weak, nonatomic) IBOutlet UISlider *mySlider;
-//テキスト
+//テキスト、TextField
 @property (weak, nonatomic) IBOutlet UITextField *myField;
 
-//カウントボタン
+//カウントボタン、countButton
 - (IBAction)add:(id)sender;
-//メッセージ表示ボタン
+//メッセージ表示ボタン、Massage button
 - (IBAction)textOn:(id)sender;
-//キーボード隠す、テキスト入力の通知の作成と実行
+//キーボード隠す、テキスト入力の通知の作成と実行、keyboad hide &Text input notice and execution 
 - (IBAction)keyHide:(id)sender;
-//スライドの値の通知の作成と実行
+//スライドの値の通知の作成と実行、slide value notice and execution
 - (IBAction)sliderA:(id)sender;
 
 
